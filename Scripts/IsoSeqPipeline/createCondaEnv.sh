16c119e53af9f9c6e5e1f40f55054e1e2acffa0f
ejh243 <e.j.hannon@exeter.ac.uk>
2021-07-16 12:00:06 +0100

    update environment

--- Scripts/IsoSeqPipeline/createCondaEnv.sh
+++ Scripts/IsoSeqPipeline/createCondaEnv.sh
@@ -1,11 +1,9 @@
 ## set up conda environment for isoseq3 pipeline
+## although tutorial for install says python 3.7, lima only works with python 2.7
 
-module load Anaconda3/5.3.0
+module load Miniconda2
 
-## had error "InvalidVersionSpecError: Invalid version spec: =2.7" which below fixed
-conda config --remove channels conda-forge
-
-conda create --name isoseq python=3.7 anaconda
+conda create --name isoseq python=2.7
 source activate isoseq
 
 conda install -n isoseq biopython
@@ -13,10 +11,9 @@ conda install -n isoseq -c http://conda.anaconda.org/cgat bx-python
 
 conda install -n isoseq -c bioconda isoseq3
 conda install -n isoseq -c bioconda pbccs
+conda install -n isoseq -c bioconda lima
 
 ## below are optional
 conda install -n isoseq -c bioconda pbcoretools # for manipulating PacBio datasets
 conda install -n isoseq -c bioconda bamtools    # for converting BAM to fasta
 conda install -n isoseq -c bioconda pysam       # for making CSV reports
-
-
