c6571a2b155aa4f23949bb37e284fd9b71d1023b
Varshini Suresh <varshiniliverpool@gmail.com>
2026-04-07 15:39:06 +0100

    add usage example with log

--- Scripts/JobSubmission/TALON/batchProcessIsoSeqData_test.sh
+++ Scripts/JobSubmission/TALON/batchProcessIsoSeqData_test.sh
@@ -13,6 +13,7 @@
 #SBATCH --array=0-34 ## runs multiple jobs with 10 at any one time
 
 # this script needs to be submitted from the main repository folder
+# Example Usage: bash Scripts/JobSubmission/TALON/batchProcessIsoSeqData_test.sh > ../isoseq_pipeline_test/logfiles/
 
 source ./Config/config.txt
 
