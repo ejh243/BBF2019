5b72f33ec537263b528fb695dd6097e0c4e718be
Varshini Suresh <varshiniliverpool@gmail.com>
2026-04-07 10:40:28 +0100

    updated software args to new isoseq tools

--- Scripts/IsoSeqPipeline/processIsoSeqSMRTcells_updated.sh
+++ Scripts/IsoSeqPipeline/processIsoSeqSMRTcells_updated.sh
@@ -1,7 +1,8 @@
-## bash script to automate processing of isoseq3 pipeline
-## assumes isoseq3 has been installed/loaded
+## bash script to automate processing of isoseq3 pipeline 
+## Updated to use the latest Isoseq3 tools - version 4.3.0 (https://isoseq.how/getting-started.html)
+## assumes isoseq3 has been installed/loaded 
 ## these steps need to be run on each file separately
-## do not store any sensitive data use config file to specify filepaths etc.
+## do not store any sensitive data use config file to specify filepaths etc. 
 ## this script requires .subread.bam, .subreads.bam.pbi, and .subreadset.xml files are located in the DATADIR
 
 cd $DATADIR/
@@ -11,59 +12,57 @@ p=$1
 echo "Processing " ${p}
 
 basename=${p%.subreads.bam}
+
+
+# Step 1: CCS - Generate circular consensus sequences (ccs) from subreads
 if [ ! -f ${PROCESSEDDIR}/CCS/${basename}.ccs.bam ] ## if final output file doesn't exist, run it through this loop
   then
   echo "File not found - Circular Consensus Sequence calling"
   ## Circular Consensus Sequence calling
-  ccs ${p} ${PROCESSEDDIR}/CCS/${basename}.ccs.bam --min-rq 0.9 --minPasses 1 --reportFile ${PROCESSEDDIR}/CCS/${basename}.ccs_report.txt
+  ccs ${p} ${PROCESSEDDIR}/CCS/${basename}.ccs.bam --min-rq 0.9 --min-passes 1 --report-file ${PROCESSEDDIR}/CCS/${basename}.ccs_report.txt
   
 else
 		echo "File Found - skipping Circular Consensus Sequence calling"
 fi
 
+
+# Step 2: Lima - Remove cDNA primers and demultiplexing barcoded data 
 if [ ! -f ${PROCESSEDDIR}/Lima/${basename}.fl.*_5p--NEB_Clontech_3p.bam ] ## if final output file doesn't exist, run it through this loop
   then
   echo "File not found - Primer removal and demultiplexing"
  	
   ## Primer removal and demultiplexing
-  lima --isoseq --peek-guess --dump-clips -j 24 ${PROCESSEDDIR}/CCS/${basename}.ccs.bam ${RESOURCESDIR}/primer.fasta ${PROCESSEDDIR}/Lima/${basename}.fl.bam 
+  lima --isoseq --peek-guess --dump-clips --num-threads 24 ${PROCESSEDDIR}/CCS/${basename}.ccs.bam ${RESOURCESDIR}/primer.fasta ${PROCESSEDDIR}/Lima/${basename}.fl.bam 
   
 else
 		echo "File Found - skipping primer removal and demultiplexing"
 fi
 
 
+# Step 3: Refine - Remove polyA and concatemers from FL reads and generate FLNC transcripts
 if [ ! -f ${PROCESSEDDIR}/Refine/${basename}.flnc.bam ] ## if final output file doesn't exist, run it through this loop
   then
   echo "File not found - refine"
  
   ## refine
-  isoseq3 refine --require-polya ${PROCESSEDDIR}/Lima/${basename}.fl.*_5p--NEB_Clontech_3p.bam ${RESOURCESDIR}/primer.fasta ${PROCESSEDDIR}/Refine/${basename}.flnc.bam
+  isoseq refine --require-polya ${PROCESSEDDIR}/Lima/${basename}.fl.*_5p--NEB_Clontech_3p.bam ${RESOURCESDIR}/primer.fasta ${PROCESSEDDIR}/Refine/${basename}.flnc.bam
   
 else
 		echo "File Found - skipping refine step"
 fi
 
+
+# Step 4: Cluster2 - Cluster FLNC reads and generate transcripts
+# Note - polish step not required in newer pipeline / cluster2 tool 
 if [ ! -f ${PROCESSEDDIR}/Cluster/clustered_${basename}.bam ] ## if final output file doesn't exist, run it through this loop
   then
   echo "File not found - Clustering "
 
-	isoseq3 cluster ${PROCESSEDDIR}/Refine/${basename}.flnc.bam ${PROCESSEDDIR}/Cluster/clustered_${basename}.bam --verbose --use-qvs --singletons --log-file ${PROCESSEDDIR}/Cluster/clustered_${basename}.log
+	isoseq cluster2 ${PROCESSEDDIR}/Refine/${basename}.flnc.bam ${PROCESSEDDIR}/Cluster/clustered_${basename}.bam --singletons --log-file ${PROCESSEDDIR}/Cluster/clustered_${basename}.log
   
 else
 		echo "File Found - skipping clustering"
 fi
 
-if [ ! -f ${PROCESSEDDIR}/Polish/polished_${basename}.singletons.bam ] ## if final output file doesn't exist, run it through this loop
-  then
-  echo "File not found - Polishing " 
-
-	isoseq3 polish ${PROCESSEDDIR}/Cluster/clustered_${basename}.bam ${DATADIR}/${basename}*.subreadset.xml ${PROCESSEDDIR}/Polish/polished_${basename}.bam
-	
-	isoseq3 polish ${PROCESSEDDIR}/Cluster/clustered_${basename}.singletons.bam ${DATADIR}/${basename}*.subreadset.xml ${PROCESSEDDIR}/Polish/polished_${basename}.singletons.bam
-
-else
-	echo "File Found - skipping polishing"
-fi
-
+# End of bulk Iso-Seq workflow. Next to continue to pigeon workflow. 
 
