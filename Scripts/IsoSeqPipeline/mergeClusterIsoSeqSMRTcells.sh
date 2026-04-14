97176c08c5310bdb6a2d75c88705d5d2dd5ebc26
Varshini Suresh <varshiniliverpool@gmail.com>
2026-04-14 09:37:47 +0100

    create script to merge and cluster isoseq data

---
+++ Scripts/IsoSeqPipeline/mergeClusterIsoSeqSMRTcells.sh
@@ -0,0 +1,6 @@
+## bash script to automate merging and clustering of processed IsoSeq data
+## Updated to use the latest Isoseq3 tools - version 4.3.0 (https://isoseq.how/getting-started.html)
+## assumes isoseq3 has been installed/loaded 
+## do not store any sensitive data use config file to specify filepaths etc.
+## this script requires .flnc.bam files are located in the ${PROCESSEDDIR}/Refine folder
+
