0300eb83cfc57494369b2bd72f6eef657aad6e2b
Varshini Suresh <varshiniliverpool@gmail.com>
2026-04-20 11:20:22 +0100

    sbatch updates

--- Scripts/IsoSeqPipeline/mergeClusterIsoSeqSMRTcells.sh
+++ Scripts/IsoSeqPipeline/mergeClusterIsoSeqSMRTcells.sh
@@ -1,3 +1,17 @@
+#!/bin/sh
+#SBATCH --export=ALL # export all environment variables to the batch job.
+#SBATCH -p pq # submit to the serial queue
+#SBATCH --time=24:00:00 # Maximum wall time for the job.
+#SBATCH -A Research_Project-193495 # research project to submit under. 
+#SBATCH --nodes=1 # specify number of nodes.
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=200G
+#SBATCH --mail-type=END # send email at job completion 
+#SBATCH --mail-user=v.suresh@exeter.ac.uk # email me at job completion
+#SBATCH --output=/lustre/home/vs455/LogFiles/Cluster2_test-%j.out
+#SBATCH --error=/lustre/home/vs455/LogFiles/Cluster2_test-%j.err
+#SBATCH --job-name=Cluster2_test
+
 ## bash script to automate merging and clustering of processed IsoSeq data
 ## Updated to use the latest Isoseq3 tools - version 4.3.0 (https://isoseq.how/getting-started.html)
 ## assumes isoseq3 has been installed/loaded 
@@ -7,36 +21,38 @@
 # If multiple SMRTcells found in PROCESSEDDIR, then create a list of all <movie>.flnc.bam using
 # This list can be used as input for cluster2 step
 
+# Usage: sbatch Scripts/IsoSeqPipeline/mergeClusterIsoSeqSMRTcells.sh
 
-p=$1
+echo "Looking for FLNC files in: ${PROCESSEDDIR}"
+mkdir -p ${PROCESSEDDIR}/Cluster2
 
-basename=$(basename "$p" .subreads.bam)
 
-echo "Looking for multiple SMRTcells in: ${PROCESSEDDIR}"
-
-if [ ! -f ${PROCESSEDDIR}/Refine/flnc.fofn ] ## if final output file doesn't exist, run it through this loop
-  then
-  echo "Ouput file not found - Running Merging step"
-
-	ls ${PROCESSEDDIR}/Refine/*.flnc.bam > flnc.fofn
-  
+# Step 1: create fofn 
+if [ ! -f ${PROCESSEDDIR}/Refine/flnc.fofn ]; then
+    echo "Creating FLNC file list"
+    find ${PROCESSEDDIR}/Refine -name "*.flnc.bam" > ${PROCESSEDDIR}/Refine/flnc.fofn
 else
-		echo "Ouput file Found - skipping merging"
+    echo "flnc.fofn exists - skipping"
 fi
  
 
 # Cluster2 - Cluster FLNC reads and generate transcripts
 # Note - polish step not required in newer pipeline / cluster2 tool 
 
-if [ ! -f ${PROCESSEDDIR}/Cluster2/clustered_flnc_fofn.bam  ## if final output file doesn't exist, run it through this loop
-  then
-  echo "Ouput file not found - Running Clustering step"
+if [ ! -f ${PROCESSEDDIR}/Cluster2/clustered_flnc_fofn.bam ]; then
+    echo "Running cluster2"
 
-	isoseq cluster2 ${PROCESSEDDIR}/Refine/flnc.fofn ${PROCESSEDDIR}/Cluster2/clustered_flnc_fofn.bam --singletons --log-file ${PROCESSEDDIR}/Cluster/clustered_flnc_fofn.log
-  
+    isoseq cluster2 \
+        ${PROCESSEDDIR}/Refine/flnc.fofn \
+        ${PROCESSEDDIR}/Cluster2/clustered_flnc_fofn.bam \
+        --singletons \
+        --log-file ${PROCESSEDDIR}/Cluster2/cluster2.log
+
 else
-		echo "Ouput file Found - skipping clustering"
+    echo "Cluster output exists - skipping"
 fi
- 
 
-# End of script tis just 
+echo "Done"
+
+
+# End of script
--- Scripts/JobSubmission/TALON/batchProcessIsoSeqData_test.sh
+++ Scripts/JobSubmission/TALON/batchProcessIsoSeqData_test.sh
@@ -6,11 +6,12 @@
 #SBATCH --nodes=1 # specify number of nodes.
 #SBATCH --ntasks-per-node=16 # specify number of processors per node
 #SBATCH --mail-type=END # send email at job completion 
-#SBATCH --mail-user=v.suresh@exeter.ac.uk # email me at job completion
-#SBATCH --output=LogFiles/PreprocessIsoseq_test-%A_%a.out
-#SBATCH --error=LogFiles/PreprocessIsoseq_test-%A_%a.err
+#SBATCH --mail-user=v.suresh@exeter.ac.uk # enter email address
+#SBATCH --output=/lustre/home/vs455/LogFiles/PreprocessIsoseq_test-%A_%a.out 
+#SBATCH --error=/lustre/home/vs455/LogFiles/PreprocessIsoseq_test-%A_%a.err 
 #SBATCH --job-name=PreprocessIsoseq_test
-#SBATCH --array=0-34%10 ## runs multiple jobs with 10 at any one time
+#SBATCH --time=05:00:00
+#SBATCH --array=0-1%2 ## runs multiple jobs with 10 at any one time (set 0-34%10 for full cohort)
 
 # this script needs to be submitted from the main repository folder
 # Example Usage: sbatch Scripts/JobSubmission/TALON/batchProcessIsoSeqData_test.sh
