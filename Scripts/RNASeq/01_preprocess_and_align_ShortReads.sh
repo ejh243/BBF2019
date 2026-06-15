a57fa7d299f529e73da1f1f023f84821ed7a5f43
Varshini Suresh <varshiniliverpool@gmail.com>
2026-06-15 17:30:01 +0100

    merge preprocessing and alignment step

--- Scripts/RNASeq/01_preprocess_and_align_ShortReads.sh
+++ Scripts/RNASeq/01_preprocess_and_align_ShortReads.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH --export=ALL # export all environment variables to the batch job.
 #SBATCH -p pq # submit to the serial queue
 #SBATCH --time=24:00:00 # Maximum wall time for the job.
@@ -7,9 +7,9 @@
 #SBATCH --ntasks-per-node=16 # specify number of processors per node
 #SBATCH --mail-type=END # send email at job completion 
 #SBATCH --mail-user=v.suresh@exeter.ac.uk # enter email address
-#SBATCH --output=/lustre/home/vs455/LogFiles/PreprocessShortReads-%A_%a.out 
-#SBATCH --error=/lustre/home/vs455/LogFiles/PreprocessShortReads-%A_%a.err 
-#SBATCH --job-name=PreprocessShortReads
+#SBATCH --output=/lustre/home/vs455/LogFiles/AlignShortReads-%A_%a.out 
+#SBATCH --error=/lustre/home/vs455/LogFiles/AlignShortReads-%A_%a.err 
+#SBATCH --job-name=AlignShortReads
 #SBATCH --array=0-19%5 ## runs multiple jobs with 5 at any one time 
 
 ## bash script to automate preprocessing of paired short read data 
@@ -17,12 +17,9 @@
     ## Configure via --array=0-N%M where N = samples-1 and M = max concurrent jobs
 
 ## do not store any sensitive data use config file to specify filepaths etc. 
-## please provide either $SHORTREADS or $SHORTREAD_LIST in the config file
-#   Assumes paired-end FASTQs are named: sample.R1.fastq.gz & sample.R2.fastq.gz
-#   If a different naming convention is used, modify lines flagged with "# must match filename pattern" 
 
 ## this script needs to be submitted from the main repository folder
-## Usage: sbatch Scripts/RNASeq/01_preprocess_ShortReads.sh
+## Usage: sbatch Scripts/RNASeq/01_preprocess_and_align_ShortReads.sh
 
 set -euo pipefail
 
@@ -31,11 +28,11 @@ echo "Job ID: ${SLURM_JOB_ID}"
 echo "Array Task ID: ${SLURM_ARRAY_TASK_ID}"
 echo ""
 
-
 ## Load required software and configurations 
 source ./Config/config.txt
 
 module load FastQC
+module load STAR
 module load Miniconda3
 source activate rnaseq_tools
 
@@ -43,64 +40,37 @@ source activate rnaseq_tools
 echo "software tools used"
 trim_galore --version
 fastqc --version
+STAR --version 
 
 
 ## Output directories 
 TRIMDIR="${RNASEQDIR}/reads_trimmed"
 FASTQC_RAW="${RNASEQDIR}/fastqc_raw"
 FASTQC_TRIMMED="${RNASEQDIR}/fastqc_trimmed"
+ALIGNEDRNA="${RNASEQDIR}/aligned_reads"
 
-mkdir -p "${TRIMDIR}"
-mkdir -p "${FASTQC_RAW}"
-mkdir -p "${FASTQC_TRIMMED}"
+mkdir -p "$TRIMDIR" "$FASTQC_RAW" "$FASTQC_TRIMMED" "$ALIGNEDRNA"
 
 
 ## Locate all input FASTQ files 
-
-# Validate input configuration 
-if [[ -n "${SHORTREADS:-}" && -n "${SHORTREAD_LIST:-}" ]]; then
-    echo "ERROR: Specify either SHORTREADS or SHORTREAD_LIST in config file, not both"
-    exit 1
-fi
-
-if [[ -z "${SHORTREADS:-}" && -z "${SHORTREAD_LIST:-}" ]]; then
-    echo "ERROR: Specify either SHORTREADS or SHORTREAD_LIST in config file"
-    exit 1
-fi
-
-
-## Read FASTQ files
-if [[ -n "${SHORTREAD_LIST:-}" ]]; then # If list is provided, check if files exist
-    if [[ ! -f "${SHORTREAD_LIST}" ]]; then
-        echo "ERROR: FASTQ list file not found:"
-        echo "${SHORTREAD_LIST}"
-        exit 1
-    fi
-
-    echo "Reading FASTQ files from list:"
-    echo "${SHORTREAD_LIST}"
-    mapfile -t ALL_FASTQS < "${SHORTREAD_LIST}"
-
-else # If filepath provided, read all FASTQ files in provided directory 
-    echo "Searching FASTQ files in:"
-    echo "${SHORTREADS}"
-    mapfile -t ALL_FASTQS < <(find "${SHORTREADS}" -maxdepth 1 -name "*.fastq.gz" | sort)
-fi
+echo "Searching FASTQ files in:"
+echo "${SHORTREADS}"
+mapfile -t ALL_FASTQS < <(find "${SHORTREADS}" -maxdepth 1 -name "*.fastq.gz" | sort)
 
 echo "Total FASTQ files found: ${#ALL_FASTQS[@]}"
 echo
 
 
 ## Build sample list using R1 FASTQ files
+# Looks for file name pattern with R1 (case insensitive)
 mapfile -t FQFILES < <(
     printf "%s\n" "${ALL_FASTQS[@]}" |
-    grep '_R1_001\.fastq\.gz$' |   # must match filename pattern 
+    grep -Ei 'r1.*\.fastq\.gz$' | 
     sort
 )
 
 echo "Number of samples found: ${#FQFILES[@]}"
 echo
-
 
 # Safety checks 
 if [[ ${#FQFILES[@]} -eq 0 ]]; then
@@ -114,16 +84,17 @@ if [[ ${SLURM_ARRAY_TASK_ID} -ge ${#FQFILES[@]} ]]; then
 fi
 
 
-# Select sample for this array
-f1="${FQFILES[$SLURM_ARRAY_TASK_ID]}"
-f2="${f1/_R1_001.fastq.gz/_R2_001.fastq.gz}" # must match filename pattern 
+## Extract R1, R2 and sample name from filename 
+f1="${FQFILES[$SLURM_ARRAY_TASK_ID]}" # Assign a sample per Slurm array 
+f2=$(echo "$f1" | sed -E 's/[Rr]1/[Rr]2/') # Match R1 filename
 
-sampleName=$(basename "${f1}" _R1_001.fastq.gz) # must match filename pattern 
+sampleName=$(basename "$f1" | sed -E 's/[._-]?[Rr]1.*\.fastq\.gz//') # Strip everything from R1 onward
 
 echo "Processing sample: ${sampleName}"
 echo "R1 FASTQ: ${f1}"
 echo "R2 FASTQ: ${f2}"
 echo
+
 
 ## Verify paired FASTQ exists 
 if [[ ! -f "${f1}" ]]; then
@@ -146,7 +117,7 @@ echo
 
 # Step 1: Run FASTQC on Raw Reads
 echo "Running FastQC on raw reads..."
-#fastqc "${f1}" "${f2}"  --threads 16  --outdir "${FASTQC_RAW}"
+fastqc "${f1}" "${f2}"  --threads 16  --outdir "${FASTQC_RAW}"
 
 
 # Step 2: Run Trim Galore 
@@ -161,5 +132,38 @@ mv "${TRIMDIR}"/*fastqc.* "${FASTQC_TRIMMED}/" 2>/dev/null || true
 
 echo
 echo "Completed preprocessing for ${sampleName}"
+
+
+# Step 4: Run STAR alignment 
+echo
+echo "Starting STAR alignment for ${sampleName}"
+
+# Locate trimmed reads dynamically
+star_f1=$(ls "${TRIMDIR}/${sampleName}"*val_1.f*q.gz 2>/dev/null | head -n 1)
+star_f2=$(ls "${TRIMDIR}/${sampleName}"*val_2.f*q.gz 2>/dev/null | head -n 1)
+
+# Validate
+[[ -f "$star_f1" ]] || { echo "ERROR: Trimmed R1 not found"; exit 1; }
+[[ -f "$star_f2" ]] || { echo "ERROR: Trimmed R2 not found"; exit 1; }
+
+echo "Using trimmed reads:"
+echo "$star_f1"
+echo "$star_f2"
+
+
+## align with STAR using GENCODE v48 star index
+STAR --genomeDir ${STARIndex} \
+    --runThreadN 18 \
+    --readFilesIn ${star_f1},${star_f2} \
+    --readFilesCommand zcat \
+    --outFileNamePrefix ${ALIGNEDRNA}/${sampleName} \
+    --outSAMtype BAM SortedByCoordinate \
+    --outSAMunmapped Within \
+    --outSAMattributes Standard
+
+echo
+echo "Completed STAR alignment for ${sampleName}"
+echo
+echo "End of script"
 
 # End of script 
